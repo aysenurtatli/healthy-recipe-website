305fd9ce7c3a51456f11345f80ad3f1d4637f6b7
Aysenur Tatli <167679785+aysenurtatli@users.noreply.github.com>
2025-08-25 20:07:00 +0300

    update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
+  experimental: {
+    appDir: true,    
+    turbo: false,     
+  },
+}
 
-export default nextConfig;
+export default nextConfig
