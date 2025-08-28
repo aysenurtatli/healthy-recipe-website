e5ffafbda9a2f246d0f5ce587f7bfa7d26a86559
Aysenur Tatli <167679785+aysenurtatli@users.noreply.github.com>
2025-08-28 14:38:17 +0300

    add cursor-pointer to button

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({btnText}) => {
   return (
-    <button className='bg-primary text-xl rounded-[10px] w-[198px] h-[60px] text-white'>{btnText}</button>
+    <button className='bg-primary text-xl rounded-[10px] w-[198px] h-[60px] text-white cursor-pointer'>{btnText}</button>
   )
 }
 
