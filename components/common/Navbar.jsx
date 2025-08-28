164225a74b3730453d6c181a9cce69373b787893
Aysenur Tatli <167679785+aysenurtatli@users.noreply.github.com>
2025-08-28 15:53:11 +0300

    add hover to navbar

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -20,15 +20,26 @@ const Navbar = () => {
       <Link href={"/"}>
        <img src="/logo.svg" alt="logo" className='w-auto'/>
       </Link>
-      <nav className='hidden lg:flex gap-[40px] font-semibold text-lg'>
-          {links.map((link) => (
-            <Link href={link.path} key={link.name} className={`${pathname === link.path ? "border-b-2 border-orange-300" : ""}`}>
-              {link.name}
-            </Link>
-          ))}
+     <nav className="hidden lg:flex gap-[40px] font-semibold text-lg">
+        {links.map((link) => (
+          <Link
+            href={link.path}
+            key={link.name}
+            className={`
+              relative pb-1
+              after:content-[''] after:absolute after:left-0 after:bottom-0 
+              after:h-[2px] after:bg-orange-300 
+              after:transition-all after:duration-300
+              ${pathname === link.path ? "after:w-full" : "after:w-0 hover:after:w-full"}
+            `}
+          >
+            {link.name}
+          </Link>
+        ))}
       </nav>
+
       <Link href={"/recipes"}>
-       <button className='hidden lg:block bg-primary cursor-pointer text-white px-4 h-[52px] text-[20px] rounded-[10px]'>Browse recipes</button>
+       <button className='hidden lg:block bg-primary cursor-pointer text-white px-4 h-[52px] text-[20px] rounded-[10px] hover:bg-secondary duration-300'>Browse recipes</button>
       </Link>
       <button
           className="lg:hidden p-2 bg-[#E0E6E3] w-10 h-10 flex items-center justify-center rounded-sm"
